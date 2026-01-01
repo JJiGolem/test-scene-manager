908a487b610deabe8feeba908a287a26ed9fc7d5
JJiGolem <lorsanov2000@gmail.com>
2026-01-01 19:18:06 +0300

    initialize project environment

---
+++ src/client/index.ts
@@ -0,0 +1,6 @@
+import { SHARED_CONSTANTS } from '@shared/constants';
+
+mp.events.add('playerReady', () => {
+	mp.console.logInfo(`${mp.players.local.name} is ready!`);
+	mp.console.logInfo(SHARED_CONSTANTS.HELLO_WORLD);
+});
